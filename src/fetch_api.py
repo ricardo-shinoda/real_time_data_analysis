30e23ef730bb85d39f97b56512892e8f1484b9fd
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-12-28 19:07:32 -0300

    feat matplotlib working

--- src/fetch_api.py
+++ src/fetch_api.py
@@ -1,11 +1,51 @@
+#!/usr/bin/env python3
 # This is going to be the place where I fetch the information from the api
 
+import matplotlib.pyplot as plt
+from datetime import datetime
+import yfinance as yf
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import yfinance as yf
-from datetime import datetime
+import matplotlib
+matplotlib.use('Qt5Agg')
 
 
 petro = yf.Ticker('petr4.sa')
-print(petro.info)
+snag11 = yf.Ticker('snag11.sa')
+klabin = yf.Ticker('klbn11.sa')
+
+ticker = klabin
+
+stockInfo = ticker.info
+
+
+for key, value in stockInfo.items():
+    print(key, ":", value)
+
+# numbShares = petro.info['sharesOutstanding']
+# currentPrice = snag11.info['currentPrice']
+# print('The number of share are: ', numbShares)
+# print('The Current Price per stock is:', currentPrice)
+
+# to check the dividends
+print(ticker.dividends)
+
+print('Current Price:', stockInfo['currentPrice'])
+# To know who the major holders are
+print(ticker.major_holders)
+
+# To know who the institutions are
+print(ticker.institutional_holders)
+
+df = petro.dividends
+data = df.resample('Y').sum()
+data = data.reset_index()
+
+data['Year'] = data['Date'].dt.year
+plt.figure()
+plt.bar(data['Year'], data['Dividends'])
+plt.ylabel('Dividend Yield ($)')
+plt.xlabel('Year')
+plt.title(f'{ticker.info["symbol"]} dividend history')
+plt.xlim(2014, 2022)
+plt.show()
