6c8db1fc176c539eadfc8bee3f586c9d202cd902
Ricardo Shinoda <ricardoshinoda@gmail.com>
2023-12-31 17:52:27 -0300

    studying basic in python

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 
+# Function
 
 def sum(value1, value2):
     result = value1 + value2
@@ -6,3 +7,23 @@ def sum(value1, value2):
 
 
 sum(1, 2)
+
+# For loop
+
+list = (1, 1, 1, 1, 1, 1)
+sum = 0
+
+for i in list:
+    sum += i
+    print('Volta', i, 'do loop. Soma:', sum)
+
+print('Fora do loop', sum)
+
+
+# Input
+teste = int(input("Teste: "))
+
+if teste >= 3:
+    print('>= 3')
+else:
+    print('not >= 3')
